452e5252a3acc5cf3c5e1f9f1dc2d365f9925512
John Shin <shinhs0506@gmail.com>
2023-04-04 02:16:50 -0700

    add wasm files

---
+++ pkg/chip8emu.d.ts
@@ -0,0 +1,71 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Chip8EmulatorWasm {
+  free(): void;
+/**
+*/
+  constructor();
+/**
+* @param {Uint8Array} data
+*/
+  init(data: Uint8Array): void;
+/**
+*/
+  emulate_cycle(): void;
+/**
+*/
+  advance_timers(): void;
+/**
+* @param {KeyboardEvent} evt
+* @param {boolean} pressed
+*/
+  keypress(evt: KeyboardEvent, pressed: boolean): void;
+/**
+*/
+  reset(): void;
+/**
+* @param {number} cell_size
+*/
+  render(cell_size: number): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_chip8emulatorwasm_free: (a: number) => void;
+  readonly chip8emulatorwasm_new: (a: number) => void;
+  readonly chip8emulatorwasm_init: (a: number, b: number) => void;
+  readonly chip8emulatorwasm_emulate_cycle: (a: number) => void;
+  readonly chip8emulatorwasm_advance_timers: (a: number) => void;
+  readonly chip8emulatorwasm_keypress: (a: number, b: number, c: number) => void;
+  readonly chip8emulatorwasm_reset: (a: number) => void;
+  readonly chip8emulatorwasm_render: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/chip8emu_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_chip8emulatorwasm_free(a: number): void;
+export function chip8emulatorwasm_new(a: number): void;
+export function chip8emulatorwasm_init(a: number, b: number): void;
+export function chip8emulatorwasm_emulate_cycle(a: number): void;
+export function chip8emulatorwasm_advance_timers(a: number): void;
+export function chip8emulatorwasm_keypress(a: number, b: number, c: number): void;
+export function chip8emulatorwasm_reset(a: number): void;
+export function chip8emulatorwasm_render(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
